31c4a987cdce73bf3b996a6ed890d4c10483e3e3
Xinyu Yang <xinyu_young@163.com>
2019-06-26 10:43:16 +0800

    netcat.cc finish version 1

--- bin/netcat.cc
+++ bin/netcat.cc
@@ -47,4 +47,57 @@ void run(TcpStreamPtr stream) {
         ::exit(0);
     });
     char buf[8192];
+    int nr = 0;
+    while ( (nr = ::read(STDIN_FILENO, buf, sizeof(buf))) > 0 ) {
+        int nw = stream->sendAll(buf, nr);
+        if (nw < nr) {
+            break;
+        }
+    }
+    stream->shutdownWrite();
+    thr.join();
+}
+
+int main(int argc, const char* argv[]) 
+{
+    if (argc < 3) {
+        printf("Usage:\n  %s hostname port\n  %s -l port\n", argv[0], argv[0]);
+        return 0;
+    }
+    
+    int port = atoi(argv[2]);
+    // if server side mode
+    if (strcmp(argv[1], "-l") == 0) {
+        std::unique_ptr<Acceptor> acceptor(new Acceptor(InetAddress(port)));
+        TcpStreamPtr stream(acceptor->accept());
+        if (stream) {
+            acceptor.reset();
+            run(std::move(stream));
+        }
+        else {
+            perror("accept connection error");
+        }
+    }
+    // client side mode 
+    else {
+        const char* hostname = argv[1];
+        InetAddress addr(port);
+        // if successful resolve a inet address
+        if (InetAddress::resolve(hostname, &addr)) {
+            TcpStreamPtr stream(TcpStream::connect(addr));
+            if (stream) {
+                run(std::move(stream));
+            }
+            // fail to connect server side
+            else {
+                printf("Not able to connect %s\n", addr.toIpPort().c_str());
+                perror("");
+            }
+        }
+        // fail to resolve inet address
+        else {
+            printf("Not able to resolve Inet Address: %s\n", hostname);
+        }
+    }
+    return 0;
 }
